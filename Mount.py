3ef025e81036802f94a1223f37a8743764f3bf6f
Alan Steremberg <alan.steremberg@gmail.com>
2018-01-22 17:25:26 -0800

    initial check in

---
+++ Mount.py
@@ -0,0 +1,63 @@
+#!/usr/bin/python3
+import os
+import shutil
+import mimetypes
+import datetime
+from mimetypes import MimeTypes
+from zipfile import ZipFile
+from flask import request, jsonify, send_file
+from werkzeug.utils import secure_filename
+from .FileManager import *
+from .HFSFileManager import *
+from .FileManagerResponse import *
+
+
+
+class Mount:
+    mounts = []
+    root = os.path.join(os.path.dirname(os.path.abspath(__file__)),'files')
+
+    # start out by initializing the root of the file system
+    def __init__(self):
+       self.mountfileunix(self.root,'MiSTer')
+       self.mountfileunix('/home/alans/Downloads','Downloads')
+       self.mountfilehfs('/home/alans/Downloads/MinivMacBootv2.dsk','MinivMacBootv2.dsk')
+       self.mountfilehfs('/home/alans/Downloads/games-o.dsk','gameso')
+
+    def getmounts(self):
+       return self.mounts
+    def lookupmount(self,folder):
+       parts    = folder.split('/')
+       mountname = parts.pop(0)
+       path     = '/'.join(parts)
+       print(mountname)
+       print(path)
+       found_mount = None
+       for m in self.mounts:
+          if m['name']==mountname:
+             found_mount=m
+             found_mount['path']=path
+       return found_mount
+          
+
+     
+    def mountfileunix(self,path,name):
+       entry = {}
+       entry['path']=path
+       entry['name']=name
+       entry['handler']=FileManager(path,name)
+       self.mounts.append(entry)
+
+    def mountfilehfs(self,path,name):
+       print('mountfilehfs: path:'+path+' name:'+name)
+       entry = {}
+       entry['path']=path
+       entry['name']=name
+       entry['handler']=HFSFileManager(path,name)
+       self.mounts.append(entry)
+
+       
+    def mountfile(self,path):
+       print(self.mounts)
+       self.mounts.append(path)
+       print(self.mounts)
