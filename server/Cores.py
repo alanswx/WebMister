6fe7d9b992c0a72497cfd7b577e6c06041b8d721
Alan Steremberg <alan.steremberg@gmail.com>
2018-02-01 06:30:49 -0800

    added regex and core reporting

--- server/Cores.py
+++ server/Cores.py
@@ -2,6 +2,7 @@ import os
 import sys
 import json
 import sys
+import re
 
 class Cores:
     manifest = {}
@@ -16,6 +17,21 @@ class Cores:
         with open(os.path.join(DIR, 'server/manifest.json'), 'r') as f:
             self.manifest = json.load(f)
 
+    def findCoreFiles(self,regex_str):
+        files = []
+        for file in os.listdir(self.mister_root):
+            matchObj = re.match(regex_str,file)
+            if (matchObj):
+               stats = os.stat(os.path.join(self.mister_root,file))
+               e = {}
+               e['target']     = file
+               e['created']     = int(stats.st_ctime)
+               e['modified']     = int(stats.st_mtime)
+               e['timestamp']     = int(stats.st_mtime)
+               e['sizeondisk']     = int(stats.st_size)
+               files.append(e)
+        return files
+
     def update_core_from_disk(self,core):
         try:
            if not self.manifest[core]:
@@ -27,11 +43,14 @@ class Cores:
         #print(core)
         #print(self.manifest[core])
         #print(self.manifest[core]['additionalData'])
+        if (self.manifest[core]['releaseFormat']):
+            files = self.findCoreFiles(self.manifest[core]['releaseFormat'])
+            coreinfo['releaseFiles']=files
         if (self.manifest[core]['additionalData']):
            directory = os.path.join(self.mister_root,self.manifest[core]['additionalDataDir'])
            additional=[]
            for e in  self.manifest[core]['additionalData']:
-              print(e)
+              #print(e)
               if (e['target']):
                   filename = os.path.join(directory,e['target'])
                   if (os.path.exists(filename)):
@@ -41,7 +60,7 @@ class Cores:
                       e['timestamp']     = int(stats.st_mtime)
                       e['sizeondisk']     = int(stats.st_size)
                       additional.append(e)
-                      print(stats)
+                      #print(stats)
            # check
            coreinfo['additionalData']=additional
         self.diskdata[core]=coreinfo
@@ -59,4 +78,5 @@ class Cores:
 
 if __name__ == "__main__":
      core = Cores(os.path.join(sys.path[0],".."),os.path.join(sys.path[0],"../../InstallerMister/misterinst"))
-     core.update_cores_from_disk()
+     results=core.update_cores_from_disk()
+     print(results)
